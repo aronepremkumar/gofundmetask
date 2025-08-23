0958c3ccfb5680122b0ba721c65e8a89f36fe509
busyarone <busyarone@gmail.com>
2025-08-23 09:24:20 -0700

    Go fund me processor processing add donor, add campaign and recurring donations

--- GoFundMeProcessor.java
+++ GoFundMeProcessor.java
@@ -1,7 +1,256 @@
 import java.io.*;
+import java.util.*;
 
 public class GoFundMeProcessor {
+    //Donors and their balances
+    private static Map<String, Double> donorBalanceMap = new HashMap<String, Double>();
+    //Campaigns and their total balances
+    private static Map<String, Double> campaignBalanceMap = new HashMap<String, Double>();
+    //Donors and their campaign amounts donated
+    private static Map<String, List<DonorCampaign>> donorCampaignMap = new HashMap<String, List<DonorCampaign>>();
+    //Campaigns and their donors amounts details
+    private static Map<String, List<CampaignDonor>> campaignDonorMap = new HashMap<String, List<CampaignDonor>>();
+    //Maps to store original names with cases
+    private static Map<String,String> donorNameMap = new HashMap<String,String>();
+    //Maps to store original campaign names with cases
+    private static Map<String,String> campaignNameMap = new HashMap<String,String>();
 
+    public static class DonorCampaign {
+        String donorName;
+        String campaignName;
+        double CampaignAmount;
+
+        public String getDonorName() {
+            return donorName;
+        }
+
+        public void setDonorName(String donorName) {
+            this.donorName = donorName;
+        }
+
+        public double getCampaignAmount() {
+            return CampaignAmount;
+        }
+
+        public void setCampaignAmount(double campaignAmount) {
+            CampaignAmount = campaignAmount;
+        }
+
+        public String getCampaignName() {
+            return campaignName;
+        }
+
+        public void setCampaignName(String campaignName) {
+            this.campaignName = campaignName;
+        }
+    }
+
+    public static class CampaignDonor {
+        String campaignName;
+        String donorName;
+        double donorAmount;
+
+        public String getCampaignName() {
+            return campaignName;
+        }
+
+        public void setCampaignName(String campaignName) {
+            this.campaignName = campaignName;
+        }
+
+        public String getDonorName() {
+            return donorName;
+        }
+
+        public void setDonorName(String donorName) {
+            this.donorName = donorName;
+        }
+
+        public double getDonorAmount() {
+            return donorAmount;
+        }
+
+        public void setDonorAmount(double donorAmount) {
+            this.donorAmount = donorAmount;
+        }
+    }
+
+    public static double computeAverage(String donorName) {
+        double average = 0.0;
+        List<DonorCampaign> donorCampaigns = donorCampaignMap.get(donorName.toLowerCase());
+        int size = donorCampaigns.size();
+        Iterator<DonorCampaign> iterator = donorCampaigns.iterator();
+        while (iterator.hasNext()){
+            DonorCampaign donorCampaign = iterator.next();
+            average += donorCampaign.getCampaignAmount();
+        }
+        return average/size;
+    }
+
+    public static void printSummary() {
+        System.out.println("Printing Summary...");
+        System.out.println("Donors:");
+        // Print donor balances
+        //order by donor name
+        //compute total and average donation per donor
+        TreeSet<String> donorSet = new TreeSet<String>(donorBalanceMap.keySet());
+        for (String donor : donorSet) {
+            System.out.println(donorNameMap.get(donor.toLowerCase()) + ": Total: $" + donorBalanceMap.get(donor) + " Average: "+computeAverage(donor));
+        }
+        System.out.println(" ");
+        // Print campaign names and their total balances
+        System.out.println("Campaigns:");
+        //order by campaign name
+        TreeSet<String> campaignSet = new TreeSet<String>(campaignBalanceMap.keySet());
+        for (String campaign : campaignSet) {
+            System.out.println(campaignNameMap.get(campaign.toLowerCase()) + ": Total: $" + campaignBalanceMap.get(campaign));
+        }
+        System.out.println("Response code: 200");
+    }
+
+    public static boolean isValidString(String name) {
+        //validate donor name
+        boolean isValid = true;
+        isValid = (name == null || name.isEmpty()) ? false : true;
+        //System.out.println("Name " + name + " is valid: " + isValid);
+        return isValid;
+    }
+
+    public static boolean isStringNumber(String str) {
+        try {
+            Double.parseDouble(str);
+            //System.out.println("String " + str + " is a valid number.");
+            return true;
+        } catch (NumberFormatException e) {
+            //System.out.println("String " + str + " is not a valid number.");
+            return false;
+        }
+    }
+
+
+    public static void processEachLine(String eachLine) {
+        System.out.println("Processing line: " + eachLine);
+        String[] parts = eachLine.split(" ");
+        String command = parts[0];
+        String type = parts[1];
+        int length = parts.length;
+        //System.out.println("Command: " + command + ", Type: " + type + ", Length: " + length);
+        if (command.equalsIgnoreCase("add") && type.equalsIgnoreCase("donor")) {
+            //Add donor case
+            if (length == 4) {
+                //validate donor name and balance
+                String donorName = parts[2];
+                String balanceStr = parts[3];
+                //if not empty string remove the dollar sign in the front
+                if (!(balanceStr == null || balanceStr.isEmpty()) && balanceStr.startsWith("$"))
+                    balanceStr = balanceStr.substring(1);
+                double balance = isStringNumber(balanceStr) ? Double.parseDouble(balanceStr) : 0;
+                if (isValidString(donorName) && balance > 0) {
+                    System.out.println("Adding donor: " + donorName + " with balance: " + balance);
+                    //add to donor balance map
+                    if(donorBalanceMap.containsKey(donorName.toLowerCase())){
+                        double existingBalance = donorBalanceMap.get(donorName.toLowerCase());
+                        balance = existingBalance + balance;
+                    }else{
+                        donorNameMap.put(donorName.toLowerCase(), donorName);
+                    }
+                    donorBalanceMap.put(donorName.toLowerCase(), balance);
+
+                } else {
+                    System.out.println("Invalid donor name or balance.");
+                }
+            } else {
+                System.out.println("Donor name or balance not found.");
+            }
+        } else if (command.equalsIgnoreCase("add") && type.equalsIgnoreCase("campaign")) {
+            //Add campaign case
+            if (length == 3) {
+                String campaignName = parts[2];
+                if (isValidString(campaignName)) {
+                    System.out.println("Adding campaign: " + campaignName);
+                    //add to campaign balance map
+                    if(!campaignBalanceMap.containsKey(campaignName.toLowerCase())){
+                        campaignBalanceMap.put(campaignName.toLowerCase(), 0.0);
+                        campaignNameMap.put(campaignName.toLowerCase(), campaignName);
+                    }
+
+                } else {
+                    System.out.println("Invalid campaign name.");
+                }
+            } else {
+                System.out.println("Invalid add campaign format.");
+            }
+        }
+        else if (command.equalsIgnoreCase("donate") && length == 4){
+            String donorName = parts[1];
+            String campaignName = parts[2];
+            String amountStr = parts[3];
+            if(!(amountStr == null || amountStr.isEmpty()) && amountStr.startsWith("$"))
+                amountStr = amountStr.substring(1);
+            double amount = isStringNumber(amountStr) ? Double.parseDouble(amountStr) :0;
+
+            //check if donor and campaign exist
+            boolean isValidDonor = (!donorBalanceMap.containsKey(donorName.toLowerCase())) ? false : true;
+            boolean isValidCampaign = (!campaignBalanceMap.containsKey(campaignName.toLowerCase())) ? false : true;
+            boolean insufficientBalance = false;
+            //check if donor has enough balance
+            if(isValidDonor){
+                double existingBalance = donorBalanceMap.get(donorName.toLowerCase());
+                insufficientBalance = (existingBalance < amount) ? true : false;
+
+            }
+
+            if(isValidDonor && isValidCampaign && !insufficientBalance && amount>0) {
+                System.out.println("Processing donation of $" + amount + " from donor " + donorName + " to campaign " + campaignName);
+                //deduct balance from donor
+                double existingBalance = donorBalanceMap.get(donorName.toLowerCase());
+                double newBalance = existingBalance - amount;
+                donorBalanceMap.put(donorName.toLowerCase(), newBalance);
+                //add balance to campaign
+                double existingCampaignBalance = campaignBalanceMap.get(campaignName.toLowerCase());
+                double newCampaignBalance = existingCampaignBalance + amount;
+                campaignBalanceMap.put(campaignName.toLowerCase(), newCampaignBalance);
+
+                //add entry to donor-campaign map
+                DonorCampaign donorCampaign = new DonorCampaign();
+                donorCampaign.setDonorName(donorName.toLowerCase());
+                donorCampaign.setCampaignName(campaignName.toLowerCase());
+                donorCampaign.setCampaignAmount(amount);
+                if(donorCampaignMap.containsKey(donorName.toLowerCase())){
+                    List<DonorCampaign> existingList = donorCampaignMap.get(donorName.toLowerCase());
+                    existingList.add(donorCampaign);
+                    donorCampaignMap.put(donorName.toLowerCase(), existingList);
+                }else{
+                    List<DonorCampaign> newList = new ArrayList<DonorCampaign>();
+                    newList.add(donorCampaign);
+                    donorCampaignMap.put(donorName.toLowerCase(), newList);
+                }
+
+                //add entry to campaign-donor map
+                CampaignDonor campaignDonor = new CampaignDonor();
+                campaignDonor.setCampaignName(campaignName.toLowerCase());
+                campaignDonor.setDonorName(donorName.toLowerCase());
+                campaignDonor.setDonorAmount(amount);
+                if(campaignDonorMap.containsKey(campaignName.toLowerCase())){
+                    List<CampaignDonor> campaignDonors = campaignDonorMap.get(campaignName.toLowerCase());
+                    campaignDonors.add(campaignDonor);
+                    campaignDonorMap.put(campaignName.toLowerCase(), campaignDonors);
+                }else {
+                    List<CampaignDonor> campaignDonors = new ArrayList<CampaignDonor>();
+                    campaignDonors.add(campaignDonor);
+                    campaignDonorMap.put(campaignName.toLowerCase(), campaignDonors);
+                }
+            }else {
+                System.out.println("Invalid donor or campaign or insufficient balance.");
+            }
+        }else{
+            System.out.println("Invalid command or format.");
+        }
+
+        //System.out.println("Donor Balances: " + donorBalanceMap);
+        //System.out.println("Campaign Balances: " + campaignBalanceMap);
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Processing GoFundMe campaigns...");
@@ -22,16 +271,19 @@ public class GoFundMeProcessor {
         }
 
         String line;
-        try{
-
+        try {
             while ((line = bufferedReader.readLine()) != null) {
-                line = line.trim();
-                if (line.isEmpty()) continue;
-                if (line.equalsIgnoreCase("END")) break;
-
-                System.out.println("Processing line: " + line);
+                try {
+                    line = line.trim();
+                    if (line.isEmpty()) continue;
+                    if (line.equalsIgnoreCase("END")) break;
+                    processEachLine(line);
+                } catch (Exception e) {
+                    System.out.println("Error processing line: " + line);
+                }
             }
-        }catch (Exception e){
+            printSummary();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
